94df062910033749c08e7c27c47e326c7151a1b5
Dodger-42 <kevin.bing.barber@gmail.com>
2020-08-05 15:13:53 -0700

    second

--- src/App.js
+++ src/App.js
@@ -1,16 +1,100 @@
 import React from 'react';
+import TodoList from './components/TodoList';
+import TodoForm from './components/TodoForm';
+
 
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+//   constructor() {
+//         super();
+//         this.state = {
+//   };
+//   }
+
+
+
+//   render() {
+//     return (
+//       <div>
+//         <h2>Welcome to your Todo App!</h2>
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
+
+
+
+  constructor() {
+    super();
+    this.state = {
+      taskArr: [{
+        task: '',
+        id: '',
+        Done: false,
+      }],
+    };
+  }
+  toggle = id => {
+    this.setState({
+      taskArr: this.state.taskArr.map(item => {
+        if(id === item.id) {
+          console.log(item)
+          return {
+            ...item,
+            done: !item.done
+          };
+        }
+        return item;
+      })
+    })
+  }
+  removeDone = event => {
+    event.preventDefault();
+    this.setState({
+      taskArr: this.state.taskArr.filter(item => item.Done === false)
+    })
+  }
+  changeHandler = event => {
+    const date = Date.now();
+    this.setState({task: event.target.value,
+                  id: date});
+  }
+  addItem = (item)  => {
+    
+    this.setState({
+      taskArr: [
+        ...this.state.taskArr,
+        {
+        task: item,
+        id: Date.now(),
+        done: false
+        }]
+    })
+    
+    console.log(this.state)
+  }
   render() {
     return (
       <div>
         <h2>Welcome to your Todo App!</h2>
+        <TodoForm addItem={this.addItem} />
+        <TodoList taskArr={this.state.taskArr} 
+                  change={this.changeHandler} 
+                  // submit={this.submitHandler} 
+                  toggle={this.toggle}
+                  removeDone={this.removeDone} />
       </div>
     );
   }
 }
-
 export default App;
+
+
+
+
+
+  
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,2 +1,22 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+
+import React from "react";
+import Todo from "./Todo";
+
+const TodoList = props => {
+  // for sorting the list based on whether an item has been done or not
+  // const sortedList = props.todo.sort((a, b) => a.done - b.done);
+  return (
+    <div className="todo-list">
+      {props.taskArr.map(todo => (
+        <Todo key={todo.id} todo={todo} toggle={props.toggle} />
+      ))}
+      <button className="clear-btn" onClick={props.removeDone}>
+        Clear Done
+      </button>
+    </div>
+  );
+};
+
+export default TodoList;
